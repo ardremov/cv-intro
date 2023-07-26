47c1845fee62321ea7fa1106cfb4e6551829dbfc
ardremov <ardremov@gmail.com>
2023-07-26 13:13:57 -0400

    lane folowin

--- lane_following.py
+++ lane_following.py
@@ -1,6 +1,20 @@
-def get_lane_center():
-    pass
+import cv2
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from dt_apriltags import Detector
+import lane_detection
 
-def recommend_direction():
-    pass
+def get_lane_center(lanes, width = 1566):
+    center_lane = int(lanes[len(lanes) / 2])
+    (slope, intercept) = lane_detection.get_slopes_intercepts(center_lane)
+    return (slope, intercept)
+        
+def recommend_direction(center, slope, width = 1566):
+    if center < width / 2:
+        return 'left'
+    elif center == width / 2:
+        return 'forward'
+    else:
+        return 'right'
 
